4f30914252d3f0982db40e089d760090866313a0
Luis Escobar <lae311@stern.nyu.edu>
2021-09-29 16:49:28 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove(main);
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory")
+newHeader.textContent = `Luis is the champion`;
+document.body.append(newHeader);
+
